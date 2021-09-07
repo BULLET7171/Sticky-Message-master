7ce5866efdffe7e1e9154ab6689a15c07b951059
BULLET7171 <73592600+BULLET7171@users.noreply.github.com>
2021-09-07 16:51:28 +0530

    stickymessage.py
    i am not really a expert-like python dev i try my best to make my own things and i made one of these in my free time

---
+++ stickymessage.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+import random
+
+
+intents = discord.Intents(messages=True, guilds=True, members=True)
+
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print("up")
+
+
+ded = []
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    for i in ded:
+        await i.delete()
+    else:
+        try:
+            if message.channel.id == 882257233872814133:
+                embed = discord.Embed()
+                #embed.add_field(name="** **",value="Please make sure not to chit chat here you can move to general if you want to talk only upload photos and stuff here")
+                embed.set_footer(text="𝙊𝙣𝙡𝙮 𝙪𝙥𝙡𝙤𝙖𝙙 𝙢𝙚𝙙𝙞𝙖 𝙝𝙚𝙧𝙚 𝙤𝙩𝙝𝙚𝙧𝙬𝙞𝙨𝙚 𝙮𝙤𝙪𝙧 𝙢𝙚𝙨𝙨𝙖𝙜𝙚 𝙬𝙞𝙡𝙡 𝙗𝙚 𝙙𝙚𝙡𝙚𝙩𝙚𝙙")
+                mes = await message.channel.send(embed=embed)
+                ded.clear()
+                ded.append(mes)
+                
+        except Exception:
+            print(Exception)
+
+client.run("token")
